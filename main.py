a9828a6a6c1787a93f8fc2e977ed335b609cec25
Zhion <zpharoah9@gmail.com>
2023-09-16 18:46:44 -0300

    Simple Recommendation function initialized

--- main.py
+++ main.py
@@ -8,9 +8,26 @@ restaurants = {
 
 }
 
-def reccomend():
-    for key in restaurants.values():
-        print(key)
-        
+def reccomend(theme):
+    theme = input("What kind of food from bermuda would you like to try?\nWe have a selction of Mexican, Japanese, Jamaican, Italian and Portugese.\nWhat would you like? ")
+    if theme == "Mexican":
+        print(f"Mexican restaurants in the area are: {restaurants['Mexican']}")
+    elif theme == "Japanese":
+        print(f"Japanese restaurants in the area are: {restaurants['Japanese']}")
+    elif theme == "Jamaican": 
+        print(f"Jamaican restaurants in the area are: {restaurants['Jamaican']}")
+    elif theme == "Italian":
+        print(f"Italian restaurants in the area are: {restaurants['Italian']}")
+    elif theme == "Portugese":  
+        print(f"Portugese restaurants in the area are: {restaurants['Portugese']}")
+    else:
+        print("No such theme exists in the recommendation system.")
+        x = input("Try again? Y/N? ")
+        if x == "y":
+            reccomend(theme)
+        else:
+            return None
+ 
 
-print(reccomend())
+
+print(reccomend(restaurants))
